d8bf2f918c49d5fea50e222c955df2f2a63a496d
ArZal6581 <ArZal6581@8R71Q52>
2026-05-28 11:33:59 -0400

    Initializing all Empty Files

--- FinalProject/src/finalproject/AbstractPiece.java
+++ FinalProject/src/finalproject/AbstractPiece.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package finalproject;
 
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
-/**
- *
- * @author NeWan5443
- */
 abstract public class AbstractPiece implements Piece {
 
     protected ArrayList<Move> validMoves;
