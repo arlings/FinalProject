314b0fb6e68c30d1969c2aad10f3aceadf3c98b4
ArZal6581 <ArZal6581@8R71Q52>
2026-05-28 10:43:18 -0400

    Initializing all Empty Files

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package finalproject;
 
-/**
- *
- * @author ArZal6581
- */
+
 public class FinalProject {
 
     /**
