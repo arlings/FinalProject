d8ade5604f6d98704436fe32f2d134fddacd6015
ArZal6581 <ArZal6581@8R71Q52>
2026-06-01 10:55:58 -0400

    Initializing all Empty Files

--- FinalProject/src/finalproject/CreateUser.java
+++ FinalProject/src/finalproject/CreateUser.java
@@ -1,46 +1,35 @@
 package finalproject;
 
-public class EnterUsername extends javax.swing.JFrame {
+public class CreateUser extends javax.swing.JFrame {
     
-    private MainWindow mainWindow;
+    MainWindow mainWindow;
     
-    public EnterUsername(MainWindow m) {
+    public CreateUser(MainWindow m) {
         initComponents();
         mainWindow = m;
-        this.setTitle("Enter username");
+        this.setTitle("Create user");
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("Enter your username");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(67, 67, 67)
-                .addComponent(jLabel1)
-                .addContainerGap(76, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(jLabel1)
-                .addContainerGap(71, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
