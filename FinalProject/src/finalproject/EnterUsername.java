21058aee6308bab24220278cdd856f943329d7b0
ArZal6581 <ArZal6581@8R71Q52>
2026-06-01 10:45:30 -0400

    Initializing all Empty Files

---
+++ FinalProject/src/finalproject/EnterUsername.java
@@ -0,0 +1,35 @@
+package finalproject;
+
+public class EnterUsername extends javax.swing.JFrame {
+    
+    private MainWindow mainWindow;
+    
+    public EnterUsername(MainWindow m) {
+        initComponents();
+        mainWindow = m;
+        this.setTitle("User information");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
