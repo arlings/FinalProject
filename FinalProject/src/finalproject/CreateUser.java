42df898186abb9576d203fab1a33018a73c910a5
ArZal6581 <ArZal6581@8R71Q52>
2026-06-04 10:48:36 -0400

    Initializing all Empty Files

--- FinalProject/src/finalproject/CreateUser.java
+++ FinalProject/src/finalproject/CreateUser.java
@@ -2,6 +2,7 @@ package finalproject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class CreateUser extends javax.swing.JFrame {
@@ -148,16 +149,26 @@ public class CreateUser extends javax.swing.JFrame {
 
     private void goBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_goBtnActionPerformed
         boolean done = false;
+        boolean hasNextLine = true;
+        
         try {
             Scanner s = new Scanner(f);
-            while (s.hasNextLine() || done) {
-                if (userNameField.getText().equalsIgnoreCase(s.nextLine().split(",")[0])) {
-                    s.close();
-                    warningWindow = new WarningWindow(this, "This username already exists");
-                    warningWindow.setVisible(true);
-                    userNameField.setText("");
-                    done = true;
+            String nextLine = "";
+            try {
+                while (hasNextLine || done)  {
+                    nextLine = s.nextLine();
+                    if (userNameField.getText().equalsIgnoreCase(nextLine.split(",")[0])) {
+                        warningWindow = new WarningWindow(this, "This username already exists");
+                        warningWindow.setVisible(true);
+                        userNameField.setText("");
+                        done = true;
+                    }
+                    if (!s.hasNextLine()) {
+                        hasNextLine = false;
+                    }
                 }
+            } catch (Exception e) {
+                
             }
         } catch (FileNotFoundException e) {
             if (warningWindow == null) {
