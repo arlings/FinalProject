0a2c7010533c40d56cbaf8fb4a5ed0a8e0fc5b5e
arlings <squidget788@gmail.com>
2026-06-05 19:08:11 -0400

    Merge origin/main into main
    # Conflicts:
    #	FinalProject/src/finalproject/MainWindow.java

---
+++ FinalProject/src/finalproject/EditPieceWindow.java
@@ -0,0 +1,45 @@
+package finalproject;
+
+public class EditPieceWindow extends javax.swing.JFrame {
+    
+    private SandboxWindow sandboxWindow;
+
+    public void MoveJFrame() {
+        this.setUndecorated(true);
+        MainWindow.FrameDragListener frameDragListener = new MainWindow.FrameDragListener(this);
+        this.addMouseListener(frameDragListener);
+        this.addMouseMotionListener(frameDragListener);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+    
+    public EditPieceWindow(SandboxWindow m, int piece) {
+        MoveJFrame();
+        initComponents();
+        sandboxWindow = m;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
